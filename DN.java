da006d3990d223f534410a2bef44e88f522604b3
Luiza <luiza.a.r@mail.ru>
2021-10-22 17:53:57 +0300

    commentary

---
+++ DN.java
@@ -0,0 +1,11 @@
+package geekbrains;
+
+import java.io.Serializable;
+
+public class DN implements Serializable {
+    public Integer Icon;
+    public String IconPhrase;
+    public Boolean HasPrecipitation;
+    public String PrecipitationType;
+    public String PrecipitationIntensity;
+}
---
+++ TemperatureValue.java
@@ -0,0 +1,9 @@
+package geekbrains;
+
+import java.io.Serializable;
+
+public class TemperatureValue implements Serializable {
+    public Double Value;
+    public String Unit;
+    public Integer UnitType;
+}
